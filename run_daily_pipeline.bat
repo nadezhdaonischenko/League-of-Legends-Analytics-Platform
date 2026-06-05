565223cca8c3db3055c9b83d51c376972fb949e8
Надежда <nadezhdaonischenko@gmail.com>
2026-06-05 10:33:17 +0600

    Update run_daily_pipeline.bat

--- run_daily_pipeline.bat
+++ run_daily_pipeline.bat
@@ -10,12 +10,12 @@ echo ============================= >> pipeline.log
 echo %date% %time% >> pipeline.log
 
 echo Запуск riot_pipeline... >> pipeline.log
-C:\ProgramData\anaconda3\python.exe riot_pipeline.py >> pipeline.log 2>&1
+C:\ProgramData\anaconda3\python.exe -u riot_pipeline.py >> pipeline.log 2>&1
 
 echo Запуск EDA... >> pipeline.log
-C:\ProgramData\anaconda3\python.exe EDA.py >> pipeline.log 2>&1
+C:\ProgramData\anaconda3\python.exe -u EDA.py >> pipeline.log 2>&1
 
 echo Запуск dashboard_etl... >> pipeline.log
-C:\ProgramData\anaconda3\python.exe dashboard_etl.py >> pipeline.log 2>&1
+C:\ProgramData\anaconda3\python.exe -u dashboard_etl.py >> pipeline.log 2>&1
 
 echo Выполнение завершено. >> pipeline.log
