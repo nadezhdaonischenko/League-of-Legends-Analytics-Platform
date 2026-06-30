5baf4aaab67579a61eced81b6edcdf93d50b0a4d
NadezhdaOnischenko <nadezhdaonischenko@gmail.com>
2026-06-30 12:10:59 +0600

    Update dashboard_etl.py

--- dashboard_etl.py
+++ dashboard_etl.py
@@ -4,22 +4,8 @@
 
 import pandas as pd
 import numpy as np
-
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
-
-load_dotenv()
-
-DB_USER = "postgres"
-DB_PASSWORD = os.getenv("parole_postgresql", "")
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "lol_db"
-
-engine = create_engine(
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+from sqlalchemy import text
+from load.db import engine
 
 def build_dashboard_tables(engine):
 
