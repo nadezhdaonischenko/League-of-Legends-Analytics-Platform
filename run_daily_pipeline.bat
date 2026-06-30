0024f14639a171745b45c85bd20d25e4deee5b5d
NadezhdaOnischenko <nadezhdaonischenko@gmail.com>
2026-06-30 12:13:40 +0600

    Update run_daily_pipeline.bat

--- run_daily_pipeline.bat
+++ run_daily_pipeline.bat
@@ -1,24 +1,13 @@
 @echo off
 
-:: UTF-8 для корректного вывода
 set PYTHONIOENCODING=utf-8
 
-:: Переход в папку проекта
 cd /d "C:\...\Project_LOL"
 
 echo ============================= >> pipeline.log
 echo %date% %time% >> pipeline.log
 
-echo Запуск EXTRACT... >> pipeline.log
-"C:\ProgramData\anaconda3\python.exe" -u main.py >> pipeline.log 2>&1
-
-echo Запуск TRANSFORM + LOAD... >> pipeline.log
-"C:\ProgramData\anaconda3\python.exe" -u run_transform_load.py >> pipeline.log 2>&1
-
-echo Запуск EDA... >> pipeline.log
-"C:\ProgramData\anaconda3\python.exe" -u EDA.py >> pipeline.log 2>&1
-
-echo Запуск DASHBOARD ETL... >> pipeline.log
-"C:\ProgramData\anaconda3\python.exe" -u dashboard_etl.py >> pipeline.log 2>&1
+echo Запуск полного ETL-пайплайна... >> pipeline.log
+"C:\ProgramData\anaconda3\python.exe" -u main.py --stage pipeline >> pipeline.log 2>&1
 
 echo Выполнение завершено. >> pipeline.log
